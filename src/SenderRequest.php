ce398a96c160ab367d471d50ec7b09f5f7360043
Renato Oda <renato.oda@onclicksistemas.com.br>
2017-08-22 18:32:35 -0300

    Criando classe para abstrair entrada de dados do facebook

---
+++ src/SenderRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace CodeBot;
+
+class SenderRequest {
+  private $event;
+
+  public function __construct() {
+    $event = file_get_contents(filename: "php://input");
+    $event = json_decode($event, assoc: true, depth:512, options: JSON_BIGINT_AS_STRING);
+    $this->event = $event['entry'][0]['messaging'][0];    
+  }
+
+  public function getSenderId() {
+    return $this->event['sender']['id'] ?? null;
+  }
+
+  public function getMessage() {
+    return $this->event['message']['text'] ?? null;
+  }
+
+  public function getPostback() {
+    if (empty($this->event['postback'])) {
+      return null;
+    }
+
+    if (is_array($this->event['postback']) and !empty(($this->event['postback']['payload'])) {
+      return $this->event['postback']['payload'];
+    }
+
+    return $this->event['postback'] ?? null;
+  }
+}
